11251c3923461d7ad0d2b628214dc11f5480f1cf
jwmahone <jwmahone@uvm.edu>
2025-01-17 16:22:48 -0500

    finalized assignment 1

--- simulate.py
+++ simulate.py
@@ -2,7 +2,7 @@ import pybullet as p
 from time import sleep
 
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0) # remove side bars
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0) # remove side bars
 for i in range(1, 1000):
     p.stepSimulation()
     sleep(1/200)
