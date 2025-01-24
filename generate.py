ecd139d10a0ca67e3d79011f90544a0bcca660c9
jwmahone <jwmahone@uvm.edu>
2025-01-24 10:53:29 -0500

    added functions to generate.py

--- generate.py
+++ generate.py
@@ -1,20 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1, 1, 1])
+    pyrosim.End()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.End()
 
-length = 1
-width = 1
-height = 1
-
-x = 0
-y = 0
-z = 0.5
-
-for i in range(0, 10):
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
-    z += height
-    length = length * 0.9
-    width = width * 0.9
-    height = height * 0.9
-
-pyrosim.End()
+Create_World()
